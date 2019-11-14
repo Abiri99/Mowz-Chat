0c732792b973e793267b4877e7ce617093a632f3
Ali-Lenovo <abiriali78@gmail.com>
2019-11-15 01:43:40 +0330

    navigation bar color changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
     SystemUiOverlayStyle(
       statusBarColor: Colors.transparent, //top bar color
       statusBarIconBrightness: Brightness.dark, //top bar icons
-      systemNavigationBarColor: Color(0xfff8f9fc), //bottom bar color
+      systemNavigationBarColor: Colors.white, //bottom bar color
       systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
     )
   );
